03da220d181e515f7692d1155fc9ef29a5c6048b
akashsekar6292 <akashsekanna@gmail.com>
2025-04-13 15:57:51 +0530

    my first dummy commit

---
+++ EmbarkXSecurityCheck1/src/main/java/com/example/EmbarkXSecurityCheck1/EmbarkXSecurityCheck1Application.java
@@ -0,0 +1,13 @@
+package com.example.EmbarkXSecurityCheck1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbarkXSecurityCheck1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbarkXSecurityCheck1Application.class, args);
+		System.out.println("ok");
+	}
+}
---
+++ EmbarkXSecurityCheck1/src/test/java/com/example/EmbarkXSecurityCheck1/EmbarkXSecurityCheck1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EmbarkXSecurityCheck1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbarkXSecurityCheck1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
